b19cbda303f329301c62085f120efb03e06424fe
mraitmaier <mraitmaier@localhost>
2012-01-20 13:40:20 +0100

    Initial version

---
+++ core/enum.py
@@ -0,0 +1,44 @@
+"""
+    enum.py - the implementation of the enums based on frozenset
+
+NOTE: this script is not to be used standalone (except for testing), import it.
+"""
+
+AUTHOR = "Miran R."
+VERSION = "1.0"
+NAME = "enumeration implementation"
+
+class enum(frozenset):
+    """The simplest class-based implemenatation of enums in python that I'm
+    aware of."""
+
+    def __getattr__(self, name):
+        if name in self:
+            return name
+        raise AttributeError("Unknown value: {}".format(name))
+
+if __name__ == "__main__":
+    print(__doc__)
+    print("Testing...")
+    TestStatus = enum(("PASS", "FAIL", "XFAIL", "NOT_TESTED", "UNKNOWN"))
+    print(TestStatus)
+    print(str(TestStatus))
+    print(repr(TestStatus))
+    print(TestStatus.PASS)
+    print(TestStatus.FAIL)
+    assert TestStatus.PASS != TestStatus.FAIL
+    assert TestStatus.PASS in TestStatus
+    print("###")
+    x = TestStatus.PASS
+    print(x)
+    if "PASS" in TestStatus:
+        print("Hura for PASS!")
+    if TestStatus.XFAIL in TestStatus:
+        print("Hura for expected fail!")
+    if "BLAH" not in TestStatus:
+        print("Boooo for Blah!")
+    try:
+        y = TestStatus.BLAH
+    except AttributeError as exc:
+        print("ERROR: {}".format(exc))
+

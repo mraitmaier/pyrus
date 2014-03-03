2fe004d06257d04365f973a1694eb224c06f79ef
Miran Raitmaier (miran raitmaier <Miran Raitmaier (miran.raitmaier@gmail.com)>
2014-03-03 13:35:23 +0100

    missed the key.py on last CI.

---
+++ web/key.py
@@ -0,0 +1,11 @@
+
+
+import base64
+
+def _generate_key():
+    '''Generate a unique session ID for a particular user.'''
+    base = base64.b64encode('This is quite long string, OK for now.')
+    # we need to replace some chars, they cannot be used in filename;
+    # currently the problem is only '/'
+    return "_%s" % base.replace('/', '$')
+
